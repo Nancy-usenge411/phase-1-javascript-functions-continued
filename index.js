35268ced8bd56a82132690291f2413d5139ed792
Nancy Usenge <nancyusenge411@gmail.com>
2024-07-04 17:59:43 +0300

    commit javascript function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+
+function saturdayFun (activity= 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (parameter='go to the office'){
+return `This Monday, I will ${parameter}.`
+}
+
+function wrapAdjective (flair = "*"){
+   return function(Adjective="a hard worker"){
+        return `You are ${flair}${Adjective}${flair}!`
+   }
+}
+const encourageingPromptFunction =wrapAdjective("%")("a dedicated programmer");
+    //=> "You are %a dedicated programmer%!"
 // code your solution here
